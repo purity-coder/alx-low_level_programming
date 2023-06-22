1b260af24c5b9e69856e0f62eb54b3c6bdb388fe
purity-coder <puritymutheu3198@gmail.com>
2023-06-22 10:47:07 -0700

    debugging project

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes the output to be an infinite loop.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("incoming infinite loop :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*[*/
+	/*  putchar(i);*/
+	/*]*/
+
+	printf("infinite loop successfuly avoided! \\o/\n");
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+This is my 0x03. c
+ debugging project
